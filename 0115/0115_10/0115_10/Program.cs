fe62ba70d47a08a23654a379f39faff38db72877
didsuaclzls <didsuaclzls1234@gmail.com>
2026-01-15 16:23:27 +0900

    0115

---
+++ 0115/0115_10/0115_10/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0115_10
+{
+    //인터페이스
+    interface IAttackable
+    {   
+        void Attack(string target);
+        int GetAttackPower();
+    }
+
+    //인터페이스 Defendable 구현
+    interface IDefendable 
+    { 
+        void Defend();
+        int GetDefensePower();
+    }
+
+
+    class Knight : IAttackable, IDefendable 
+    {
+
+        public string name;
+        public int attackPower;
+        public int defensePower;
+
+        public Knight()
+        {
+            name = "검사";
+            attackPower = 10;
+        }
+        public void Attack(string target)
+        {
+            Console.WriteLine(target+"을(를) 검으로 공격");
+        }
+
+        public void Defend()
+        {
+            Console.WriteLine("기사가 방어모드중");
+        }
+
+        public int GetAttackPower()
+        {
+            return attackPower;
+        }
+
+        public int GetDefensePower()
+        {
+            return defensePower;
+        }
+    }
+    class Magician : IAttackable 
+    {
+        public string name;
+        public int MP;
+        public int spellPower;
+        public Magician()
+        {
+            name = "마법사";
+            MP = 100;
+            spellPower = 100;   
+        }
+        public void Attack(string target) 
+        {
+            Console.WriteLine(target + "을(를) 마법으로 공격");
+        }
+
+        public int GetAttackPower()
+        {
+            return MP * spellPower;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.Attack("오크");
+            knight.Defend();
+
+            Magician magician = new Magician();
+            magician.Attack("슬라임");
+
+            IAttackable[] attacker = new IAttackable[2];
+            attacker[0] = new Knight();
+            attacker[1] = new Magician();
+
+            IDefendable defender = new Knight();
+            defender.Defend();
+
+            foreach(var atk in attacker)
+            {
+                atk.Attack("고블린");
+            }
+        }
+    }
+}
---
+++ 0115/0115_11/0115_11/Field.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _0115_11
+{
+    public class Field
+    {
+        Player m_pPlayer;
+        Monster m_pMonster;
+
+        public void SetPlayer(Player pPlayer) {  m_pPlayer = pPlayer; }
+        public void Progress()
+        {
+            //사냥터
+            int iInput = 0;
+            while (true) 
+            {
+                Console.Clear();
+                m_pPlayer.Render();
+
+                DrawMap();
+                iInput=int.Parse(Console.ReadLine());
+                if (iInput == 4) break;
+                if (iInput <= 3)
+                {
+                    CreateMonster(iInput);
+                    Fight();
+                }
+            }
+        }
+        //팩토리 메서드 패턴
+        public void Create(string _strName, int _iHp, int _iAttack, out Monster pMonster)
+        {
+            pMonster = new Monster(); //몬스터생성
+            INFO tMonster = new INFO(); //몬스터데이터 메모리 주기 객체 생성
+
+            tMonster.strName = _strName; //초보 몬스터 ,중, 고
+            tMonster.iHp = _iHp; //체력
+            tMonster.iAttack = _iAttack;
+
+            pMonster.SetMonster(tMonster); //생성된 데이터 INFO 클래스타입 데이터 세팅
+        }
+
+
+        public void CreateMonster (int input)
+        {
+            switch(input)
+            {
+                case 1:
+                    //공장처럼 찍어내기
+                    //팩토리 메서드 패턴
+                    Create("초보몹", 30, 3,out m_pMonster);
+                    break;
+                case 2:
+                    Create("중수몹", 60, 6, out m_pMonster);
+                    break;
+                case 3:
+                    Create("고수몹", 90, 9, out m_pMonster);
+                    break;
+            }
+        }
+        public void Fight()
+        {
+            int iInput = 0;
+
+            while (true)
+            {
+                Console.Clear(); //콘솔화면 지움
+                m_pPlayer.Render(); //플레이어 정보 출력
+                m_pMonster.Render(); //몬스터 정보출력
+
+                Console.WriteLine("1.공격 2.도망 : "); //1,2선택 표시
+                iInput = int.Parse(Console.ReadLine());
+
+                if (iInput == 1)
+                {
+                    //플레이어 데미지 주기 몬스터 공격력 넣어서 데미지
+                    m_pPlayer.SetDamage(m_pMonster.GetMonster().iAttack);
+                    //몬스터 데미지 주기 플레이어 공격력 넣기
+                    m_pMonster.SetDamage(m_pPlayer.GetInfo().iAttack);
+
+                    if (m_pPlayer.GetInfo().iHp <= 0) //플레이어 체력이 0이하이면
+                    {
+                        m_pPlayer.SetHp(100);
+
+                        Console.WriteLine("유다이");
+                        Thread.Sleep(1000);
+                        break;
+                    }
+
+
+                }
+
+
+                if (iInput == 2 || m_pMonster.GetMonster().iHp <= 0)
+                {
+                    m_pMonster = null; //참조일때 null 초기화하면 가비지컬렉터에서 나중에 알아서 메모리 삭제
+                    break;
+                }
+            }
+        }
+
+        public void DrawMap()
+        {
+            Console.WriteLine("1. 초보맵");
+            Console.WriteLine("2. 중수맵");
+            Console.WriteLine("3. 고수맵");
+            Console.WriteLine("4. 전단계");
+            Console.WriteLine("===============");
+            Console.WriteLine("맵을 선택하세요 : ");
+        }
+    }
+}
